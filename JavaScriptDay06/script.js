863bd3874399f0821d01d4d01b9b09b174b4302a
enchilders <enchilders@hotmail.com>
2015-04-20 15:41:17 -0700

    firstCommit

---
+++ JavaScriptDay06/script.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('app', []);
+
+//Takes 2 parameters:
+//1 - name of the controller
+//2 - function to hold the controller itself
+app.controller('HomeController', function ($scope) {
+    //$scope.Task is the value from our input
+    $scope.tasks = [];
+    $scope.tasks[0] = "test";
+    //pulls the value from the input, and pushes it into our array
+    $scope.addTask = function () {
+        $scope.tasks.push($scope.Task);
+        $scope.Task = '';
+    };
+});
